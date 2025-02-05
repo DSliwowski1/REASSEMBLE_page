2a501fc1a309ffbe9784aabdf6b52b0fc5269146
Sliwowski, Daniel Jan <daniel.sliwowski@tuwien.ac.at>
2025-02-05 08:58:39 +0100

    added new recording

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { WebViewer } from "@rerun-io/web-viewer";
 // First, ensure your .rrd files are copied to dist/data during build
 const rrdFiles = [
   "./2025-01-11-14-04-40.rrd",
+  "./2025-01-09-15-27-49.rrd",
   // "./data/file2.rrd",
   // Add paths to all your .rrd files
 ];
